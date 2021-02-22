7b562cb0ac8329d79fb73a2a7f6086d6d3c7d3b9
p-flock <pflock@bu.edu>
2021-02-21 22:38:12 -0500

    add insert, delete, and save functionality

--- src/editor.rs
+++ src/editor.rs
@@ -55,7 +55,7 @@ impl Editor {
 
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
-        let mut initial_status = String::from("HELP: Ctrl-Q = quit");
+        let mut initial_status = String::from("HELP: Ctrl-S = Save | Ctrl-Q = quit");
         let document = if args.len() > 1{
             let file_name = &args[1];
             let doc = Document::open(&file_name);
@@ -82,6 +82,26 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Ctrl('s') => {
+                if self.document.save().is_ok() {
+                    self.status_message =
+                        StatusMessage::from("File saved sucessfully".to_string());
+                } else {
+                    self.status_message = StatusMessage::from("Error writing file!".to_string());
+                }
+            },
+            Key::Char(c) => {
+                self.document.insert(&self.cursor_position, c);
+                self.move_cursor(Key::Right);
+            },
+            Key::Delete => self.document.delete(&self.cursor_position),
+            Key::Backspace => {
+                if self.cursor_position.x > 0 || self.cursor_position.y
+                    > 0 {
+                        self.move_cursor(Key::Left);
+                        self.document.delete(&self.cursor_position);
+                    }
+            },
             Key::Up
             | Key::Down
             | Key::Left
